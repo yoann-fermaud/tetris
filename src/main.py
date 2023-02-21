d0934ad12eb7589fdd174501065c1f2c192f9b10
yoann-fermaud <yoann.fermaud@laplateforme.io>
2023-02-21 10:49:18 +0100

    [ADD] game over menu

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import sys
 import pathlib
-from src.tetris import *
+from src.tetris import Tetris, Text
 from src.settings import *
+
+from src.button import Button
 
 
 class App:
@@ -16,9 +18,10 @@ class App:
         self.text = Text(self)
 
     def load_images(self):
-        files = [item for item in pathlib.Path(SPRITE_DIR_PATH).rglob('*.png') if item.is_file()]
-        images = [pg.image.load(file).convert_alpha() for file in files]
-        images = [pg.transform.scale(image, (TILE_SIZE, TILE_SIZE)) for image in images]
+        images = []
+        for i in range(7):
+            image = pg.image.load(SPRITE_DIR_PATH + str(i) + ".png")
+            images.append(image)
         return images
 
     def set_timer(self):
@@ -56,11 +59,58 @@ class App:
 
     def run(self):
         while True:
-            self.check_events()
-            self.update()
+            if self.tetris.game_over_flag:
+                Endgame().run()
+                self.tetris.game_over_flag = False
+            else:
+                self.check_events()
+                self.update()
+                self.draw()
+
+
+class Endgame:
+    def __init__(self):
+        pg.init()
+        pg.display.set_caption("TETRIS")
+        self.screen = pg.display.set_mode(WIN_RES)
+        self.app = App()
+
+        self.button_play = Button(image=None, pos=(WIN_RES[0] / 2, 500), text_input="PLAY AGAIN",
+                                  font=60, base_color="white", hovering_color=(233, 135, 193))
+        self.button_quit = Button(image=None, pos=(WIN_RES[0] / 2, 600), text_input="QUIT",
+                                  font=60, base_color="white", hovering_color=(233, 135, 144))
+
+    def events(self):
+        self.mouse_pos = pg.mouse.get_pos()
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                if self.button_play.checkForInput(self.mouse_pos):
+                    self.app.run()
+                elif self.button_quit.checkForInput(self.mouse_pos):
+                    pg.quit()
+                    sys.exit()
+
+    def draw(self):
+        background = pg.image.load("assets/background/background_menu.png")
+        self.screen.blit(background, (0, 0))
+
+        self.text = pg.font.Font(FONT_PATH, 80)
+        self.text_rect = self.text.render("GAME OVER", True, (233, 175, 135))
+        self.screen.blit(self.text_rect, (WIN_RES[0] / 2 - self.text_rect.get_width() / 2, 225))
+
+        self.button_play.update(self.screen)
+        self.button_quit.update(self.screen)
+        pg.display.flip()
+
+    def update(self):
+        self.button_play.changeColor(self.mouse_pos)
+        self.button_quit.changeColor(self.mouse_pos)
+
+    def run(self):
+        while True:
+            self.events()
             self.draw()
-
-
-# if __name__ == '__main__':
-#     app = App()
-#     app.run()
+            self.update()
